6d0e709316c05f91dfc4bf8f69e12e8ee9f164c8
Fahim MIS <fahim.wakrah@gmail.com>
2025-08-27 13:53:52 +0600

    implemented Nav Link to the project with some CSS tweaks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
             <p>This is Header
-                
             </p>
+            <nav>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/mobiles'>Mobiles</NavLink>
+                <NavLink to='/laptops'>Laptops</NavLink>
+                <NavLink to='/about'>About us</NavLink>
+            </nav>
         </div>
     );
 };
