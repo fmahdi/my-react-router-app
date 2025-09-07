02c77851ec919e7731b1e695cae46cfdaf66e594
fahim2k25 <fahim.program@gmail.com>
2025-09-07 23:45:04 +0600

    added new hooks like useNavigate

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import "./Post.css"
 
 const Post = ({ elem }) => {
     const { title, id } = elem;
+    const navigate = useNavigate();
+    const handleNavigateClick = () => {
+        navigate(`/posts/${id}`);
+    }
     return (
         <div className='card-post'>
             <h3>{title}</h3>
@@ -12,7 +16,7 @@ const Post = ({ elem }) => {
                 <button>SHow Details</button>
             </Link>
             {/* Navigation way 2 */}
-            <button>Details of Post ID-{id}</button>
+            <button onClick={handleNavigateClick}>Details of Post ID-{id}</button>
         </div>
     );
 };
--- src/components/PostDeetz/PostDeetz.jsx
+++ src/components/PostDeetz/PostDeetz.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 // import '../Post/Post.css'
 
 const PostDeetz = () => {
     const { userId, id, title, body } = useLoaderData();
+    const navigate = useNavigate();
     return (
         <div className='card-post'>
             <h3>{title}</h3>
             <p>{body}</p>
             <p><small>This post is made by user no. {userId}</small></p>
+            <button onClick={() => navigate(-1)}>Go Back</button>
         </div>
     );
 };
