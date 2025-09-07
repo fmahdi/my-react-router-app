be8c14fe8186aae45af387db987cebd2f85ad4f5
fahim2k25 <fahim.program@gmail.com>
2025-09-08 00:49:50 +0600

    added a navigation lazy loading spinner for content load buffer

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SideBar from '../Sidebar/SideBar';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import './Root.css';
 
 
 
 const Root = () => {
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location);
+
     return (
         <div>
             <Header></Header>
             <div className='flex-me'>
                 <SideBar></SideBar>
+                {isNavigating && <h2>~~~~~Loading~~~~~</h2>}
                 <Outlet></Outlet>
             </div>
             <Footer></Footer>
